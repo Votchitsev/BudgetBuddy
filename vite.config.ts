8e8f8674adec2fa62d2c34775fd6cbb50a16e4a7
Dmitry <dmitry.votchitsev@gmail.com>
2024-06-01 16:14:37 +0200

    feat: add pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,24 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 import tsConfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsConfigPaths()],
+  plugins: [
+    react(),
+    tsConfigPaths(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: 'Budget Buddy',
+        short_name: 'Budget Buddy',
+        theme_color: '#ffffff',
+        icons: [],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@app': '/src/app/',
