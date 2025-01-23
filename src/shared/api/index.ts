493a9294e8e9839e7b9be2cc7822310a61b4f92a
Dmitry <dmitry.votchitsev@gmail.com>
2025-01-23 19:11:40 +0200

    fix: base url

--- src/shared/api/index.ts
+++ src/shared/api/index.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const api = createApi({
   reducerPath: 'api',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://45.140.146.226:5002/' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'https//votchitsev.fun/api/budget-buddy/' }),
   tagTypes: ['PlannedBudget', 'Income', 'DailyExpense', 'Savings'],
   endpoints: (builder) => ({
     signUp: builder.mutation({
